74993d8c65b5da0c1924a60d4462523f7009a293
Dolba420 <151568@csgaugustinus.nl>
2020-09-02 14:24:36 +0000

    jaman

--- JS/H2/H2O13.js
+++ JS/H2/H2O13.js
@@ -1,5 +1,5 @@
-var aantalRijenRaster = 6;
-var aantalKolommenRaster = 9;
+var aantalRijenRaster = 12;
+var aantalKolommenRaster = 18;
 var celGrootte;
 
 var animatie = [];
@@ -12,6 +12,10 @@ var yJos = 300;
 
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+for (var a = 0;a < aantalBeeldjes;a++) {
+    frame = loadImage("images/sprites/Jos100px/Jos_" + a + ".png");
+    animatie.push(frame);
+  }
   frame = loadImage("images/sprites/Jos100px/Jos_0.png");
 }
 
@@ -21,28 +25,34 @@ function setup() {
   frameRate(10);
   celGrootte = width / aantalKolommenRaster;
 }
+var animatieframe = 3;
 
 function draw() {
+  
   background(brug);
   tekenRaster();
 
   if (keyIsDown(LEFT_ARROW)) {
     xJos -= celGrootte;
+    animatieframe = 2;
   }
   if (keyIsDown(RIGHT_ARROW)) {
     xJos += celGrootte;
+    animatieframe = 1;
   }
   if (keyIsDown(UP_ARROW)) {
     yJos-=celGrootte;
+        animatieframe = 4;
   }
   if (keyIsDown(DOWN_ARROW)) {
     yJos += celGrootte;
+    animatieframe = 5;
   }
   
   xJos = constrain(xJos,0,width - celGrootte);
   yJos = constrain(yJos,0,height - celGrootte);
   
-  image(frame,xJos,yJos);
+  image(animatie[animatieframe],xJos,yJos,celGrootte,celGrootte);
 }
 
 function tekenRaster() {
