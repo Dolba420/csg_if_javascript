d80f4161bc2980e660dcf25bf6381d75f9e4763f
Dolba420 <151568@csgaugustinus.nl>
2020-10-28 20:46:05 +0000

    grfgdgd

--- mijnGame_copy/JS/mijnGame.js
+++ mijnGame_copy/JS/mijnGame.js
@@ -9,6 +9,7 @@ class ballen {
         this.y = random(20,300);
         this.diameter = 70;
         this.id = balnr;
+        balnr++;
     }
 
     teken(){
@@ -16,13 +17,20 @@ class ballen {
         ellipse(this.x, this.y, this.diameter);
     }
     check(){
-        for(var x = 0; x<aantal; x++){
+    for(var x = 0; x<aantal; x++){
         if(dist(mouseX,mouseY,this.x,this.y) < this.diameter/2){
             raak = true;
         }
         else{
             raak = false;
         }
+    }
+    if(raak == true){
+        aantalraak++;
+        bal[this.id] = null;
+    }
+    else{
+        aantalmis++;
     }
     }
 }
@@ -31,38 +39,12 @@ class ballen {
 
 function mouseClicked() {
 for(var x = 0; x<aantal; x++){
-  bal[x].check();
+    bal[x].check();
   }
 }
 
 
-/*class Boom {
-  constructor() {
-  this.leeftijd = round(random(1,5));
-  this.R = random(100,200);
-  this.G = 155;
-  this.B = random(0,100);
-  this.x = random(50,720);
-  }
-  
-  groei() {
-    if (this.leeftijd < 20) {
-      this.leeftijd++;
-    }
-  }
 
-  teken() {
-    push();
-    translate(this.x,375);
-    fill('sienna');
-    rect(0,0,20 + 2*this.leeftijd,-100 - 10*this.leeftijd);
-    fill(this.R,this.G,this.B);
-    ellipse((20 + 2*this.leeftijd) / 2,-90 -5*this.leeftijd,80 + 4*this.leeftijd,130 + 10*this.leeftijd);
-    pop();
-  }
-}*/
-
-// we maken een lege array waar alle objecten in komen te staan
 var bal = [];
 
 
@@ -76,17 +58,17 @@ function setup() {
   bal.push(new ballen());
   }
 
- /* for (var b = 0; b < 20; b++) {
-    bomen.push(new Boom());
-  }*/
 }
 
 function draw() {
   background("#b3b3b3");
 for(var x = 0; x<aantal; x++){
-  bal[x].teken();
-  }
+  if(bal[x] == null){
+    bal[x] = new ballen();
   
+  }
+   bal[x].teken();
+}
   //bomen[0].check();
   //bomen[10].G = 0;
   //fill('wheat');
