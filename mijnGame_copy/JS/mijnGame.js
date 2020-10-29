37baea34d4cc346cf767d5a10886e93f059fe61d
Dolba420 <151568@csgaugustinus.nl>
2020-10-29 15:06:55 +0000

    a sagbi

--- mijnGame_copy/JS/mijnGame.js
+++ mijnGame_copy/JS/mijnGame.js
@@ -1,52 +1,67 @@
 var balnr = 0;
 var aantalraak = 0;
 var aantalmis = 0;
-var aantal = 3;
+var aantal = 5;
+var elementen = [];
 var raak = null;
-class ballen {
+var accuracy;
+var time = 60;
+var start = false;
+var crosshairpositie = [];
+
+class bal {
     constructor(){
-        this.x = random(20,300);
-        this.y = random(20,300);
+        this.x = round(random(1,7),0)*70;
+        this.y = round(random(1,7),0)*70;
         this.diameter = 70;
         this.id = balnr;
         balnr++;
     }
-
     teken(){
         fill("#00eeff");
         ellipse(this.x, this.y, this.diameter);
+        fill("black");
+        textSize(32);
+    if(aantalraak != 0){
+    accuracy = (aantalraak/(aantalraak+aantalmis));
+        text("accuracy " + round(accuracy*100,0), 200,30);
+        text("0:" + round(time,0), 450,30);
+        text("score " + round(aantalraak*accuracy*500,0), 700,30);
     }
-    check(){
-    for(var x = 0; x<aantal; x++){
-        if(dist(mouseX,mouseY,this.x,this.y) < this.diameter/2){
-            raak = true;
-        }
-        else{
-            raak = false;
-        }
     }
-    if(raak == true){
-        aantalraak++;
-        bal[this.id] = null;
+
+}
+class game {
+constructor(){
+    this.yes = true;
     }
-    else{
-        aantalmis++;
-    }
+    endscreen(){
+
+        background("#003e8f");
+        text("score " + round(aantalraak*accuracy*1000,0), 30,30);
+        text("accuracy " + round(accuracy*100,0), 30,60);
     }
 }
 
 
+var spel = new game();
 
 function mouseClicked() {
-for(var x = 0; x<aantal; x++){
-    bal[x].check();
-  }
+    if(start == true){
+    for(var x = 0; x<aantal; x++){
+        if(dist(elementen[x].x, elementen[x].y,mouseX,mouseY) < elementen[x].diameter/2){
+            elementen[x] = new bal();
+            aantalraak++;
+            raak = true;
+        }
+    }
+    if(raak != true){
+        aantalmis++;
+    }
+    raak = false;
 }
-
-
-
-var bal = [];
-
+start = true;
+}
 
 
 function setup() {
@@ -54,31 +69,25 @@ function setup() {
   canvas.parent('processing');
   noStroke();
   frameRate(60);
-  for(var x = 0; x<aantal; x++){
-  bal.push(new ballen());
-  }
-
+    for(var x = 0; x<aantal; x++){
+        elementen[x] = new bal();
+    }
 }
 
 function draw() {
   background("#b3b3b3");
-for(var x = 0; x<aantal; x++){
-  if(bal[x] == null){
-    bal[x] = new ballen();
-  
-  }
-   bal[x].teken();
-}
-  //bomen[0].check();
-  //bomen[10].G = 0;
-  //fill('wheat');
-  //rect(0,350,canvas.width,canvas.height - 350);
-
-
-  /*for (var n = 0;n < bomen.length;n++) {
-    if (bomen[n].leeftijd < 10) {
-      bomen[n].teken();
-      bomen[n].groei();
+    if(start == true){
+    for(var x = 0; x<aantal; x++){
+        elementen[x].teken();
     }
-  }*/
+      time -= (1/60);
+    }
+    if(round(time,0) == 0){
+        start = false;
+        spel.endscreen();
+    }
+crosshairpositie[0] = mouseX
+crosshairpositie[1] = mouseY
+
 }
+
